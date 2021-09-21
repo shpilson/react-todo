24a963221f8f8172201d0473567f7566ccf9003a
shpilson <kovalenko.jora@gmail.com>
2021-09-21 17:24:21 +0300

    update

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import listSvg from './assets/img/list.svg'
+import List from './components/List'
 
 function App() {
   return (
     <div className="todo">
       <div className="todo__sidebar">
-        <ul className="todo__list">
-          <li className="active">
-          <i>
-            <img src={listSvg} alt="List icon" />
-          </i>
-            <span>Все задачи</span>
-          </li>
-        </ul>
+      <List />
       </div>
       <div className="todo__tasks"></div>
     </div>
