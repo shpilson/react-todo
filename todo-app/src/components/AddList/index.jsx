eb92564c23e218f9c59d69092ac62ba81462b332
shpilson <kovalenko.jora@gmail.com>
2021-10-03 20:44:58 +0300

    update

--- todo-app/src/components/AddList/index.jsx
+++ todo-app/src/components/AddList/index.jsx
@@ -13,7 +13,7 @@ const AddList = ({ colors, onAdd }) => {
 
     const addList = () => {
         if (!inputValue) {
-            alert('Введите название списка');
+            alert('Введите название спискa');
             return;
         }
         onAdd({
